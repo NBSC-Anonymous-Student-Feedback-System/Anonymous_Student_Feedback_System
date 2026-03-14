d7938e847f9c5045751f4324a02b30613499389b
FrancisAdrianIdul33 <20231685@nbsc.edu.ph>
2026-03-15 04:02:33 +0800

    Changes in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 <?php
-// includes/footer.php
 function renderFooter() {
-    echo '</body></html>';
+    echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>';
 }

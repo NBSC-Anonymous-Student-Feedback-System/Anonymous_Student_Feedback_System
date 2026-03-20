b0f57c0f84aea0c35c3e72e1c7d66bc13a56c293
FrancisAdrianIdul33 <20231685@nbsc.edu.ph>
2026-03-21 07:45:13 +0800

    include folder removed

--- includes/activity-logger.php
+++
@@ -1,5 +0,0 @@
-<?php
-// includes/activity-logger.php
-// Standalone helper – just include and call logActivity()
-// Already defined in config/functions.php; this file is kept for direct inclusion if needed.
-require_once __DIR__ . '/../config/function.php';
